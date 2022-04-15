32cf09d2f5fd8f8df52aaa18a2ce90f6a9365681
marygirl92 <lolyz19@gmail.com>
2022-04-15 09:36:54 -0500

    aprendiendo PHP

---
+++ variables.php
@@ -0,0 +1,14 @@
+<?php
+
+$numero_1 = 8;
+$numero_2 = 7;
+
+echo $numero_1 + $numero_2;
+
+define("NUMERO_PI", 3.14);
+// define("NUMERO_PI", 14);
+
+const numero_PI = 3.14;
+echo numero_PI;
+
+echo "\n";
